93a49dac86a1448ff3e71fcc7bc070de435fa72a
Courtney Stow <courney-stow@hotmail.co.uk>
2022-11-23 15:00:49 +0000

    Updated functions.py

--- functions.py
+++ functions.py
@@ -3,20 +3,24 @@
 
 # def is a keyword that marks the start of a function header. The header always consists of the function name 
 # e.g.
+
 def my_function():
     '''
     Docstring: Documentation about the function.
     '''
     print("Hello from a function")
 
+
 # Calling a function
 my_function() # This prints "Hello from a function"
+
 
 # Functions that take arguments
 def my_function_with_args(name):
     print("Hello " + name)
     
 my_function_with_args("John Smith") # This prints "Hello John Smith"
+
 
 # Using return instead of print
 def add_function(num1, num2):
@@ -27,3 +31,15 @@ print(result) # This prints 3
 
 result2 = add_function(5, 10)
 print(result2) # This prints 15
+
+
+def say_hello(name):
+    return f"Hello {name}"
+print(say_hello("John")) # This prints "Hello John"
+# print(say_hello()) You will get a TypeError: say_hello() missing 1 required positional argument: 'name'
+
+# You can provide a default value to an argument by using the assignment operator (=).
+def say_hello(name="Default"):
+    return f"Hello {name}"
+print(say_hello("John")) # This prints "Hello John"
+print(say_hello()) # This prints "Hello Default"
