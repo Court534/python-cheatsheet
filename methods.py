b7c5cf4ea8ab44db932136103b0bd93bcfbe33ac
Courtney Stow <courney-stow@hotmail.co.uk>
2022-11-23 13:58:49 +0000

    Created a new file methods.py

---
+++ methods.py
@@ -0,0 +1,11 @@
+# A method is a function that is associated with an object.
+
+mylist = [1, 2, 3]
+
+# .append() adds an item to the end of a list
+mylist.append(4)
+print(mylist) # This will print [1, 2, 3, 4]
+
+# .pop() removes the last item from a list
+mylist.pop()
+print(mylist) # This will print [1, 2, 3]
