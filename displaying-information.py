33b2bc8a5e195fbf0c7c0bd49579a7298688506e
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-02-28 15:10:30 +0000

    Added getting user information

--- displaying-information.py
+++ displaying-information.py
@@ -18,3 +18,13 @@ display(row1, row2, row3)
 row2[1] = "X"
 
 display(row1, row2, row3)
+
+# Accepting user input
+result = input("Please enter a value: ")
+print(result) # The output will always be a string
+print(type(result))
+
+# If you wanted the output to be another value type, you would have to convert it
+result_int = int(result)
+print(result_int)
+print(type(result_int))
