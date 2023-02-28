7747a5709e53ace3f5711113af1019799e3412e9
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-02-28 16:07:50 +0000

    Created a while loop

--- displaying-information.py
+++ displaying-information.py
@@ -20,20 +20,35 @@ row2[1] = "X"
 display(row1, row2, row3)
 
 # Accepting user input
-result = input("Please enter a value: ")
-print(result) # The output will always be a string
-print(type(result))
+
+#result = input("Please enter a value: ")
+#print(result) # The output will always be a string
+# print(type(result))
 
 # If you wanted the output to be another value type, you would have to convert it
-result_int = int(result)
-print(result_int)
-print(type(result_int))
+
+# result_int = int(result)
+# print(result_int)
+# print(type(result_int))
 
 # How to control user input
-def user_choice():
+
+# def user_choice():
     
-    choice = input("Please neter an number (0-10): ")
+#     choice = input("Please neter an number (0-10): ")
+    
+#     return int(choice)
+
+# The issue with the above is that it will accept any input, even if it is not a number, and in that case will throw an error
+
+# We can use a while loop to keep asking the user for input until they give us a valid input
+
+def user_choice():
+    choice = "WRONG"
+    
+    while choice.isdigit() == False:
+        choice = input("Please enter an number (0-10): ")
     
     return int(choice)
 
-# The issue with the above is that it will accept any input, even if it is not a number and will throw an error
+user_choice()
