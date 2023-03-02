278f8e481d30a0f6a8fc3a3a6e20cb5cd2ca20ad
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-03-02 14:59:14 +0000

    Small python coding challenge

---
+++ list_game.py
@@ -0,0 +1,9 @@
+# Simple interacted program that allows a user to:
+# Display a list
+# Choose an index position and input a value
+# Replace value at index position with a user's chosen input 
+
+def display_game(game_list):
+    print("Here is the current list: ")
+    print(game_list)
+
