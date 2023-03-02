7c61954f5fc96da78710cad9534d3c098f3ffcd6
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-03-02 16:00:07 +0000

    Created a new file and noted down the user criteria

---
+++ mile-stone-project1.py
@@ -0,0 +1,8 @@
+# Tic Tac Toe
+
+# User criteria:
+# 2 players should be able to play (on the same computer)
+# The board should be printed everytime a player makes a move
+# Need to be able to accept input of the player postition and then place a 
+# symbol on the board
+
