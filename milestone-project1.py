e944ad2dc3a3f6cc4168be8e956f31c3372f5304
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-03-02 16:37:29 +0000

    Renamed the milestone project1 file

--- milestone-project1.py
+++ milestone-project1.py
@@ -6,3 +6,17 @@
 # Need to be able to accept input of the player postition and then place a 
 # symbol on the board
 
+def display_board(board):
+  
+  board = ([" ", " ", " "
+         " ", " ", " "
+         " ", " ", " "])
+  
+  print("Here is the current board: ")
+  print(board)
+  
+  display_board(board)
+  
+def player_input():
+  
+  acceptable_inputs = ["X", "O"]
