7e4f693a4e4709eb64d9ebd90bbf60698462554b
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-04-05 13:48:03 +0100

    Created a basic layout of a class

---
+++ oop.py
@@ -0,0 +1,13 @@
+# Brief over view of what a class looks like
+class NameOfClass():
+    
+    def __init__(self, param1, param2):
+        self.param1 = param1
+        self.param2 = param2
+        
+    def some_method(self):
+        # perfrom some action
+        print(self.param1)
+        
+
+        
