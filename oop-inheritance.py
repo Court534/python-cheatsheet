40d72076f9f887924458c0f9dba094f8afba5fef
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-04 13:52:34 +0100

    Added comments to oop-inheritance

--- oop-inheritance.py
+++ oop-inheritance.py
@@ -14,11 +14,20 @@ myanimal.who_am_i()
 
 myanimal.eat()
 
+
+# This class is inheriting from the above class Animal
 class Dog(Animal):
     def __init__(self):
         Animal.__init__(self)
         print("Dog created")
+     
+     # We can overwrite the existing fuctions we've inherited simply by calling them the same name   
+    def who_am_i(self):
+        print("I am a dog")
         
 mydog = Dog()
+
+# Calling the the renamed function
+mydog.who_am_i()
     
         
