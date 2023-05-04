59a2a78ec455d5be6adea3bf09c9b18dede4e065
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-04 13:41:57 +0100

    new oop file added

---
+++ oop-inheritance.py
@@ -0,0 +1,16 @@
+class Animal():
+    def __init__(self):
+        print("Animal crated!")
+        
+    def who_am_i(self):
+        print("I am an animal")
+        
+    def eat(self):
+        print("I am eating")
+        
+myanimal = Animal()
+
+myanimal.who_am_i()
+
+myanimal.eat()
+        
