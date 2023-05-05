44618aadea10d299347b01d51553eb2663b436b1
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-05 16:09:35 +0100

    Added comments

--- oop-polymorphism.py
+++ oop-polymorphism.py
@@ -1,17 +1,22 @@
+# Dog class with a speak function 
 class Dog():
     
     def __init__(self, name):
         self.name = name 
         
     def speak(self):
-        return self.name + "says Woof!"
+        return self.name + " says Woof!"
     
+# Cat class that is identical to the dog class    
 class Cat():
     def __init__(self, name):
         self.name = name
         
-    def speak():
-        return self.name + "say Meow!"
+    def speak(self):
+        return self.name + " says Meow!"
     
 niko = Dog("Niko")
 felix = Cat("Felix")
+
+print(niko.speak())
+print(felix.speak())
