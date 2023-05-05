da574a1366013b451493b159399a2f3d6889cf1d
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-05 16:07:08 +0100

    Created two classes that are almost idenitcal

---
+++ oop-polymorphism.py
@@ -0,0 +1,17 @@
+class Dog():
+    
+    def __init__(self, name):
+        self.name = name 
+        
+    def speak(self):
+        return self.name + "says Woof!"
+    
+class Cat():
+    def __init__(self, name):
+        self.name = name
+        
+    def speak():
+        return self.name + "say Meow!"
+    
+niko = Dog("Niko")
+felix = Cat("Felix")
