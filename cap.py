ea468ab499db585765fe4932bddb4320aa281e84
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-06 17:42:16 +0100

    Amended test file

--- cap.py
+++ cap.py
@@ -2,7 +2,7 @@ def cap_text(text):
     
     ''' Takes a string and will capitalize the first letter '''
     
-    return text.capitalize()
+    return text.title()
 
 print(cap_text("hello"))
     
