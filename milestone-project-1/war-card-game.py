102a6045631a8de1d9596a49686b30dd8498df59
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-09 10:44:55 +0100

    Moved the values dictionary outside the card class

--- milestone-project-1/war-card-game.py
+++ milestone-project-1/war-card-game.py
@@ -1,11 +1,12 @@
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 
+            'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
 class Card:
     
     def __init__(self, suite, rank):
         self.suite = suite
         self.rank = rank
+        self.values = values[rank]
         
     def __str__(self):
         return self.rank + " of " + self.suite
     
-    values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 
-            'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
