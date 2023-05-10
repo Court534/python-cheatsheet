207e73ef7c5267b8d6e46abaff6bfc902ca2cf4e
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-10 12:58:06 +0100

    Created card and deck classes

--- milestone-project-2/blackjack.py
+++ milestone-project-2/blackjack.py
@@ -22,3 +22,34 @@ suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10,
          'Queen': 10, 'King': 10, 'Ace': 11}
+
+playing = True 
+
+# Card class
+class Card:
+    
+    def __init__(self, suits, rank):
+        self.suits = suit
+        self.rank = rank
+        
+    def __str__(self):
+        return self.rank + " of " + self.suits
+    
+# Deck class
+class Deck:
+    
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for k in rank:
+                create_card = Card(suits, rank)
+                self.deck.append(create_card)
+                
+    def shuffle(self):
+        random.shuffle(self.deck)
+        
+    def deal_one(self):
+        return self.deck.pop()
+        
+        
+        
